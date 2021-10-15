b0945b6b789edbcabc8e837978ab9729eed53d98
nate2411 <nathandj28@gmail.com>
2021-10-15 11:52:30 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Nathan Reaction timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <results v-if="showResults" :score="score"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import block from './components/block.vue'
+import results from './components/results.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { block , results},
+  data(){
+    return{
+      isPlaying:false,
+      delay:null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods:{
+    start(){
+     this.delay = 2000 + Math.random() * 5000
+     this.isPlaying = true
+     console.log(this.delay)
+    },
+    endGame(reactionTime){
+  this.score = reactionTime
+  this.isPlaying = false 
+  this.showResults= true
+
+  }
+  
   }
 }
 </script>
@@ -20,7 +43,27 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: grey;
   margin-top: 60px;
 }
+
+button{
+  background: green;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+
+button[disabled]{
+opacity: 0.2;
+cursor: not allowed;
+
+}
+
+
 </style>
